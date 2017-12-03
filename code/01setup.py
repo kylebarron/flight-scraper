88b41ce5fe4269e3dd5cfd63db85de936b4eac01
Kyle Barron <barronk@mit.edu>
2017-12-03 15:32:28 -0500

    Update 01setup to download phantomjs

--- code/01setup.py
+++ code/01setup.py
@@ -10,62 +10,81 @@ import sys
 import platform
 import zipfile
 import stat
+import shutil
 
 # Get airports data
 def get_airport_data():
-    colnames = ["openflights_id", "airport_name", "city", "country", "iata", "icao", "lat", "lon", "altitude", "timezone", "dst", "tz_zone", "type", "source"]
-    airports = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat",
+    colnames = ['openflights_id', 'airport_name', 'city', 'country', 'iata', 'icao', 'lat', 'lon', 'altitude', 'timezone', 'dst', 'tz_zone', 'type', 'source']
+    airports = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat',
                            header = None,
                            names = colnames)
-    airports = airports.loc[:, ["city", "country", "iata", "lat", "lon", "altitude", "tz_zone"]]
-    airports = airports[airports.iata != "\\N"]
-    airports = airports[airports.country.isin(["United States", "Canada"])]
-    feather.write_dataframe(airports, "../data/airports.feather")
+    airports = airports.loc[:, ['city', 'country', 'iata', 'lat', 'lon', 'altitude', 'tz_zone']]
+    airports = airports[airports.iata != '\\N']
+    airports = airports[airports.country.isin(['United States', 'Canada'])]
+    feather.write_dataframe(airports, '../data/airports.feather')
 
 def get_chromedriver():
-    latest_chromedriver_version = requests.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE").text[:-1]
+    latest_chromedriver_version = requests.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE').text[:-1]
 
-    if not os.path.exists("../bin"):
-        os.makedirs("../bin")
+    if not os.path.exists('../bin'):
+        os.makedirs('../bin')
 
-    if sys.platform.startswith("linux"):
-        if platform.architecture()[0] == "64bit":
-            url = "https://chromedriver.storage.googleapis.com/" + latest_chromedriver_version + "/chromedriver_linux64.zip"
+    if sys.platform.startswith('linux'):
+        if platform.architecture()[0] == '64bit':
+            url = 'https://chromedriver.storage.googleapis.com/' + latest_chromedriver_version + '/chromedriver_linux64.zip'
         else:
-            url = "https://chromedriver.storage.googleapis.com/" + latest_chromedriver_version + "/chromedriver_linux32.zip"
-    elif sys.platform == "darwin":
-        url = "https://chromedriver.storage.googleapis.com/" + latest_chromedriver_version + "/chromedriver_mac64.zip"
+            url = 'https://chromedriver.storage.googleapis.com/' + latest_chromedriver_version + '/chromedriver_linux32.zip'
+    elif sys.platform == 'darwin':
+        url = 'https://chromedriver.storage.googleapis.com/' + latest_chromedriver_version + '/chromedriver_mac64.zip'
     else:
-        url = "https://chromedriver.storage.googleapis.com/" + latest_chromedriver_version + "/chromedriver_win32.zip"
+        url = 'https://chromedriver.storage.googleapis.com/' + latest_chromedriver_version + '/chromedriver_win32.zip'
 
     chromedriver = requests.get(url)
-    with open("../bin/chromedriver.zip", "wb") as f:
+    with open('../bin/chromedriver.zip', 'wb') as f:
         f.write(chromedriver.content)
 
-    chromedriver_zip = zipfile.ZipFile("../bin/chromedriver.zip", "r")
-    if sys.platform == "win32":
-        chromedriver_zip.extract("chromedriver.exe", "../bin/")
+    chromedriver_zip = zipfile.ZipFile('../bin/chromedriver.zip', 'r')
+    if sys.platform == 'win32':
+        chromedriver_zip.extract('chromedriver.exe', '../bin/')
     else:
-        chromedriver_zip.extract("chromedriver", "../bin/")
+        chromedriver_zip.extract('chromedriver', '../bin/')
     chromedriver_zip.close()
-    os.remove("../bin/chromedriver.zip")
+    os.remove('../bin/chromedriver.zip')
     # Make executable for user
-    st = os.stat("../bin/chromedriver")
-    os.chmod("../bin/chromedriver", st.st_mode | stat.S_IXUSR)
+    st = os.stat('../bin/chromedriver')
+    os.chmod('../bin/chromedriver', st.st_mode | stat.S_IXUSR)
 
 def get_phantomjs():
-    if not os.path.exists("../bin"):
-        os.makedirs("../bin")
+    if not os.path.exists('../bin'):
+        os.makedirs('../bin')
 
-    if sys.platform.startswith("linux"):
-        if platform.architecture()[0] == "64bit":
-            url = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
+    if sys.platform.startswith('linux'):
+        if platform.architecture()[0] == '64bit':
+            url = 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2'
         else:
-            url = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2"
-    elif sys.platform == "darwin":
-        url = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip"
+            url = 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2'
+    elif sys.platform == 'darwin':
+        url = 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip'
     else:
-        url = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip"
+        url = 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip'
+
+    phantomjs = requests.get(url)
+    with open('../bin/phantomjs.zip', 'wb') as f:
+        f.write(phantomjs.content)
+
+    phantomjs_zip = zipfile.ZipFile('../bin/phantomjs.zip', 'r')
+    if sys.platform == 'win32':
+        phantomjs_zip.extract('bin/phantomjs.exe', '../bin/')
+    else:
+        phantomjs_zip.extract('phantomjs-2.1.1-macosx/bin/phantomjs', '../bin/')
+    phantomjs_zip.close()
+    os.rename('../bin/phantomjs-2.1.1-macosx/bin/phantomjs', '../bin/phantomjs')
+    os.remove('../bin/phantomjs.zip')
+    shutil.rmtree('../bin/phantomjs-2.1.1-macosx')
+    
+    # Make executable for user
+    st = os.stat('../bin/phantomjs')
+    os.chmod('../bin/phantomjs', st.st_mode | stat.S_IXUSR)
 
 
 get_airport_data()
